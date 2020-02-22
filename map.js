9fb80561b0aa224cb0b8e6cdb372ac1148cc9b04
NoCode <36163454+taojunzh@users.noreply.github.com>
2020-02-22 16:13:26 -0500

    added functions to compute and display a list of tiles #14

--- map.js
+++ map.js
@@ -16,18 +16,6 @@ function Tile(zoom, x, y){
 }
 }
 
-let center = getTile(3, 42.8863889, -78.8786111);
-var zoom=center.getZoom();
-var x=center.getX();
-var y=center.getY();
-console.log(y);
-console.log(x);
-console.log(zoom);
-var img = document.createElement("IMG");
-//img.setAttribute("src", "http://a.tile.openstreetmap.org/"+zoom+"/"+x+"/"+y+".png");
-img.setAttribute("src", center.getURL());
-document.body.appendChild(img);
-
 function getTile(zoom, long, lati){
   longitudeInRadians = Math.PI * long / 180.0;
   latitudeInRadians = Math.PI * lati / 180.0;
@@ -44,3 +32,50 @@ function getTile(zoom, long, lati){
   y *= howManyTilesAtThisZoomLevel;
   return new Tile(zoom, parseInt(x), parseInt(y));
 }
+
+function tile_list(center_tile){
+  let tiles=[];
+  let zoom=center_tile.getZoom();
+  let x=center_tile.getX();
+  let y=center_tile.getY();
+  if(zoom==0) {
+  for(var i =0; i<9; i++) { tiles[i]=new Tile(zoom, x, y);}}
+  if(zoom==1){
+    tiles=[
+      new Tile(zoom, 0,1),
+      new Tile(zoom, 1,1),
+      new Tile(zoom, 0,1),
+      new Tile(zoom, 0,0),
+      center_tile,
+      new Tile(zoom, 0,0),
+      new Tile(zoom, 0,1),
+      new Tile(zoom, 1,1),
+      new Tile(zoom, 0,1),
+    ];
+  }else{
+    tiles=[
+      new Tile(zoom, x-1,y-1),
+      new Tile(zoom, x,y-1),
+      new Tile(zoom, x+1,y-1),
+      new Tile(zoom, x-1,y),
+      center_tile,
+      new Tile(zoom, x+1,y),
+      new Tile(zoom, x-1,y+1),
+      new Tile(zoom, x,y+1),
+      new Tile(zoom, x+1,y+1),
+    ];
+  }
+  return tiles;
+}
+
+function display(tiles){
+  for (var i=0;i<tiles.length;i++){
+    let img = document.createElement("IMG");
+    img.setAttribute("src", tiles[i].getURL());
+    document.body.appendChild(img);
+  }
+}
+
+let center = getTile(3, 42.8863889, -78.8786111);
+let list = tile_list(center);
+display(list);
