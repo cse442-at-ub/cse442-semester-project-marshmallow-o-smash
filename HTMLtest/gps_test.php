a655b77613241bd6871c500edf5ec9ea56d37a90
unknown <ldong7@buffalo.edu>
2020-03-29 16:26:02 -0400

    GPS added#36

--- HTMLtest/gps_test.php
+++ HTMLtest/gps_test.php
@@ -72,6 +72,7 @@ li a:hover {
   padding: 10px 10px;
   margin-top: 3px;
   font-size: 17px;
+  border: inset;
   opacity:0.9;
   display: block;
   margin-right: 3px;
@@ -100,23 +101,6 @@ li a:hover {
   transform:translate(0px,45px);
 
 }
-.selectbar{
-	display: inline-block;
-	margin:3px 3px 3px 3px;
-	postion:relative;
-}
-.selectbar div{
-	display: inline-block;
-	line-height:30px;
-	color: #173660;
-	font-size: 13px;
-	font-weight: bold;
-}
-.selectbar select{
-	padding: 5px 5px;
-	font-size: 13px;
-	opacity:0.8;
-}
 </style>
 </head>
 <body>
@@ -134,15 +118,7 @@ li a:hover {
     <form method ="POST" action="">
 	  <input type="text" id = "from" list="buildings" placeholder="Starting Location" name="start">
 	  <input type="text" id = "to" list="buildings" placeholder="Destination" name="dest">
-	  <div class="selectbar">
-		<div>Route Option:</div>
-		<select id="options">
-			<option value="shortest">Shortest Route</option>
-			<option value="outdoor">Outdoor Route</option>
-			<option value="tunnel">Tunnel Route</option>
-		</select>
-	  </div>
-		 <datalist id="buildings">
+	  	  <datalist id="buildings">
 			<option value="Alfiero Center">
 			<option value="Alumni Arena">
 			<option value="Baird Hall">
@@ -175,7 +151,7 @@ li a:hover {
 			<option value="Student Union">
 			<option value="Talbert Hall">
 	    </datalist>
-       <input type="submit" name="search" style="color: white; background:#176BE2;" value="Go!">
+       <input type="submit" name="search" style="color: white;background:#176BE2;" value="Go!">
     </form>
 	<?php
 	$conn= mysqli_connect("tethys.cse.buffalo.edu:3306","yingyinl","50239602");
@@ -186,7 +162,7 @@ li a:hover {
     ?>
 			<script>
 				var str="<?php echo $message?>";
-				//alert(str);
+				alert(str);
 			</script>
 	<?php
 }
@@ -221,6 +197,7 @@ li a:hover {
   </div>
   </div>
   <button onclick="getLocation()">GPS</button>
+
   <script>
   var map=L.map('mapid').setView([42.9997, -78.7857], 16);
     L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -235,16 +212,16 @@ li a:hover {
 	var dlon = document.getElementById("d").getAttribute("destlon");
 	var dname= document.getElementById("d").getAttribute("destname");
 
-  if (cname&&dname){
-  	// L.marker([clat,clon]).addTo(map).bindPopup("Current Location").openPopup();
-  	// L.marker([dlat,dlon]).addTo(map).bindPopup(dname).openPopup();
-    getRoute(clat,clon,cname,dlat,dlon,dname);
+  	if (cname&&dname){
+  		// L.marker([clat,clon]).addTo(map).bindPopup("Current Location").openPopup();
+  		// L.marker([dlat,dlon]).addTo(map).bindPopup(dname).openPopup();
+      getRoute(clat,clon,cname,dlat,dlon,dname);
 
-  var test_dname = dname+": ("+dlat+","+dlon+")";
-  var test_cname = "Starting Location"+": ("+clat+","+clon+")";
+  	var test_dname = dname+": ("+dlat+","+dlon+")";
+  	var test_cname = "Starting Location"+": ("+clat+","+clon+")";
 
-  L.marker([clat,clon]).addTo(map).bindPopup(test_cname).openPopup();
-  L.marker([dlat,dlon]).addTo(map).bindPopup(test_dname).openPopup();
+  		L.marker([clat,clon]).addTo(map).bindPopup(test_cname).openPopup();
+  		L.marker([dlat,dlon]).addTo(map).bindPopup(test_dname).openPopup();
 	}
 
 
@@ -282,7 +259,6 @@ li a:hover {
   }
 
 
-
   /********For GPS*********/
   var glat;
   var glon;
@@ -301,6 +277,7 @@ li a:hover {
     glon=position.coords.longitude;
     L.marker([glat,glon]).addTo(map);
   }
+
 
 
 </script>
